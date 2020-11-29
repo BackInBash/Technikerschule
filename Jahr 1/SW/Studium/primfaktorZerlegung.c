0bc60e57e8a7260e05da5d114653376bf859b11c
Markus <48181660+BackInBash@users.noreply.github.com>
2020-11-29 20:22:03 +0100

    Changed Algorithm

--- Jahr 1/SW/Studium/primfaktorZerlegung.c
+++ Jahr 1/SW/Studium/primfaktorZerlegung.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
@@ -23,26 +24,35 @@ int main()
     int vektor_count = 0;
 
     // Berechnung Primfaktoren
-    while(t*t <= input)
-    {
-        if(input % t == 0){
-            // Speichern des Primfaktors im Array
-            vektor[vektor_count] = t;
-            // Array Index erhöhen
-            vektor_count++;
 
-            input = input / t;
-        }
-        else
-        {
-            t++;
-        }
+    while (input % 2 == 0) 
+    {
+        // Speichern des Primfaktors im Array
+        vektor[vektor_count] = 2;
+        // Array Index erhöhen
+        vektor_count++;
+
+        input = input /= 2;
     }
 
-    // Speichern des Primfaktors im Array
-    vektor[vektor_count] = t;
-    // Array Index erhöhen
-    vektor_count++;
+    for (int i = 3; i <= sqrt(input); i += 2) 
+    {
+            while (input % i == 0) {
+                // Speichern des Primfaktors im Array
+                vektor[vektor_count] = i;
+                // Array Index erhöhen
+                vektor_count++;
+                input = input /= i;
+            }
+    }
+
+    if (input > 2) 
+    {
+        // Speichern des Primfaktors im Array
+        vektor[vektor_count] = input;
+        // Array Index erhöhen
+        vektor_count++;
+    }
 
     // Ausgabe der Werte des Arrays
     for(int i = 0; i<vektor_count; i++)
