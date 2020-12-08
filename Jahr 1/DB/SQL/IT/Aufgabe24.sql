b6a07ab63dbeceebac750439ed8a18ab11d3b4cb
Markus <48181660+BackInBash@users.noreply.github.com>
2020-12-08 13:07:43 +0100

    Update Aufgabe24.sql

--- Jahr 1/DB/SQL/IT/Aufgabe24.sql
+++ Jahr 1/DB/SQL/IT/Aufgabe24.sql
@@ -1,5 +1,6 @@
-SELECT sum(a4.einstandspreis-a3.auftragspreis) as Gewinn
+SELECT a3.auftragspreis-a4.einstandspreis as Gewinn
 FROM auftraege a2, auftragspositionen a3, artikel a4 
 WHERE a2.auftragsnummer = a3.auftragsnummer 
 AND a3.artikelnummer = a4.ArtikelNr
-AND a2.auftragsnummer = "VA00-006";
+AND a2.auftragsnummer = "VA00-006"
+GROUP BY a2.auftragsnummer;
