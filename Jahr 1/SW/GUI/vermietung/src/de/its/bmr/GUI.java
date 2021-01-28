40c980d672e6c2756193c00ac212ef65c973a920
Markus <48181660+BackInBash@users.noreply.github.com>
2021-01-28 12:18:28 +0100

    Added Ferienwohnung

--- Jahr 1/SW/GUI/vermietung/src/de/its/bmr/GUI.java
+++ Jahr 1/SW/GUI/vermietung/src/de/its/bmr/GUI.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package de.its.sw;
+package de.its.bmr;
 
 import javax.swing.JTabbedPane;
 
 /**
  *
- * @author lorenz
+ * @author lorenz & markus
  */
 public class GUI extends javax.swing.JFrame {
 
@@ -56,6 +56,21 @@ public class GUI extends javax.swing.JFrame {
         lb_z_error = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
         jLabel4 = new javax.swing.JLabel();
+        txf_fwnr = new javax.swing.JTextField();
+        jLabel11 = new javax.swing.JLabel();
+        jLabel12 = new javax.swing.JLabel();
+        jLabel13 = new javax.swing.JLabel();
+        jLabel14 = new javax.swing.JLabel();
+        cb_fw_fruehstueck = new javax.swing.JCheckBox();
+        cb_fw_behindert = new javax.swing.JCheckBox();
+        txf_fw_anzahlwochen = new javax.swing.JTextField();
+        jLabel16 = new javax.swing.JLabel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        txa_fw_anzeigekosten = new javax.swing.JTextArea();
+        bt_fw_berechnekosten = new javax.swing.JButton();
+        txf_fw_anzahlpers = new javax.swing.JTextField();
+        jLabel15 = new javax.swing.JLabel();
+        txf_fw_gebuehr = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -140,7 +155,7 @@ public class GUI extends javax.swing.JFrame {
                                 .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 173, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                                 .addComponent(txf_z_anzahltage)))))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 29, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 35, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel10, javax.swing.GroupLayout.PREFERRED_SIZE, 136, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
@@ -159,7 +174,6 @@ public class GUI extends javax.swing.JFrame {
                     .addComponent(txf_zimmernr, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(3, 3, 3)
                 .addComponent(jLabel10)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
@@ -193,23 +207,124 @@ public class GUI extends javax.swing.JFrame {
 
         jTabbedPane1.addTab("Zimmer", jPanel1);
 
-        jLabel4.setText("jLabel4");
+        jLabel4.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
+        jLabel4.setText("Ferienwohnung");
+
+        jLabel11.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
+        jLabel11.setText("behindertengerecht");
+
+        jLabel12.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
+        jLabel12.setText("Anzahl Wochen");
+
+        jLabel13.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
+        jLabel13.setText("Frühstück");
+
+        jLabel14.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
+        jLabel14.setText("Max Personen");
+
+        jLabel16.setFont(new java.awt.Font("Dialog", 0, 18)); // NOI18N
+        jLabel16.setText("Kosten");
+
+        txa_fw_anzeigekosten.setColumns(20);
+        txa_fw_anzeigekosten.setRows(5);
+        jScrollPane2.setViewportView(txa_fw_anzeigekosten);
+
+        bt_fw_berechnekosten.setText("berechne Kosten");
+        bt_fw_berechnekosten.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bt_fw_berechnekostenActionPerformed(evt);
+            }
+        });
+
+        jLabel15.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
+        jLabel15.setText("Endreinigungsbebühr");
+
+        txf_fw_gebuehr.setText("40");
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(17, 17, 17)
-                .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(540, Short.MAX_VALUE))
+                .addGap(29, 29, 29)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addComponent(jLabel4)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(txf_fwnr, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(15, 15, 15)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(jPanel2Layout.createSequentialGroup()
+                                .addComponent(jLabel11, javax.swing.GroupLayout.PREFERRED_SIZE, 173, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(cb_fw_behindert, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(jPanel2Layout.createSequentialGroup()
+                                .addComponent(jLabel14, javax.swing.GroupLayout.PREFERRED_SIZE, 173, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(txf_fw_anzahlpers, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(jPanel2Layout.createSequentialGroup()
+                                .addComponent(jLabel13, javax.swing.GroupLayout.PREFERRED_SIZE, 173, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(cb_fw_fruehstueck, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(jPanel2Layout.createSequentialGroup()
+                                .addComponent(jLabel12, javax.swing.GroupLayout.PREFERRED_SIZE, 173, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(txf_fw_anzahlwochen, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(jPanel2Layout.createSequentialGroup()
+                                .addComponent(jLabel15, javax.swing.GroupLayout.PREFERRED_SIZE, 173, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(txf_fw_gebuehr, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jLabel16, javax.swing.GroupLayout.PREFERRED_SIZE, 136, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(192, 192, 192))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 339, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                                .addComponent(bt_fw_berechnekosten, javax.swing.GroupLayout.PREFERRED_SIZE, 137, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(91, 91, 91)))
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(28, 28, 28)
-                .addComponent(jLabel4)
-                .addContainerGap(337, Short.MAX_VALUE))
+                .addGap(25, 25, 25)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(txf_fwnr, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(3, 3, 3)
+                .addComponent(jLabel16)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(cb_fw_behindert)
+                            .addComponent(jLabel11, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(15, 15, 15)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel13, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(cb_fw_fruehstueck))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel14, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txf_fw_anzahlpers, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(12, 12, 12)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel15, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txf_fw_gebuehr, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel12, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txf_fw_anzahlwochen, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(5, 5, 5)
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 162, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(bt_fw_berechnekosten)))
+                .addContainerGap(103, Short.MAX_VALUE))
         );
 
         jTabbedPane1.addTab("Ferienwohnung", jPanel2);
@@ -220,7 +335,7 @@ public class GUI extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 99, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 126, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(txf_fruehstueckspreis, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(63, 63, 63))
@@ -251,18 +366,14 @@ public class GUI extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-    public boolean checkparams() {
+    public boolean checkparams(javax.swing.JTextField o1, javax.swing.JTextField o2) {
         //bt_z_berechnekosten.setEnabled(false);
-        boolean b;
+        boolean b = true;
         
-            if (txf_z_anzahltage.getText().isEmpty() || (txf_zimmernr.getText().isEmpty())) {
+            if (o1.getText().isEmpty() || (o2.getText().isEmpty())) {
                 lb_z_error.setText("Bitte füllen Sie Zimmernummer und Tage aus");
                 
                 b = false;
-
-            } else {
-                lb_z_error.setText("");
-                b = true;
 
             }
         
@@ -272,10 +383,11 @@ public class GUI extends javax.swing.JFrame {
         // TODO add your handling code here:
         double ergebnis;
         
-        if (checkparams() == true) {
+        if (checkparams(txf_zimmernr, txf_z_anzahltage) == true) {
+            
+            Unterkunft.setFruehstueckspreis(Integer.parseInt(txf_fruehstueckspreis.getText()));
             
             Zimmer zimmer1 = new Zimmer(cb_z_meerblick.isSelected(), Integer.parseInt(txf_zimmernr.getText()), cb_z_behindert.isSelected());
-            Unterkunft.setFruehstueckspreis(Integer.parseInt(txf_fruehstueckspreis.getText()));
             zimmer1.setAnzahlTage(Integer.parseInt(txf_z_anzahltage.getText()));
             zimmer1.setAlsEinzelzimmer(cb_z_einzelzimmer.isSelected());
             zimmer1.setFruehstueck(cb_z_fruehstueck.isSelected());
@@ -287,6 +399,26 @@ public class GUI extends javax.swing.JFrame {
         }
 
     }//GEN-LAST:event_bt_z_berechnekostenActionPerformed
+
+    private void bt_fw_berechnekostenActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bt_fw_berechnekostenActionPerformed
+        // TODO add your handling code here:
+        double ergebnis;
+        
+        if (checkparams(txf_fwnr, txf_fw_anzahlwochen) == true) {
+            
+            Ferienwohnung.setWochenpreis(685);
+            Ferienwohnung.setEndreinigungsgebühr(Integer.parseInt(txf_fw_gebuehr.getText()));
+            Unterkunft.setFruehstueckspreis(Integer.parseInt(txf_fruehstueckspreis.getText()));
+            
+            Ferienwohnung fw1 = new Ferienwohnung(Integer.parseInt(txf_fw_anzahlpers.getText()), Integer.parseInt(txf_fwnr.getText()), cb_z_behindert.isSelected());
+            fw1.setAnzahlWochen(Integer.parseInt(txf_fw_anzahlwochen.getText()));
+            fw1.setFruehstueck(cb_fw_fruehstueck.isSelected());
+            ergebnis = fw1.uebernachtungspreisBerechnen();
+            
+            txa_fw_anzeigekosten.setText("Dieses Ferienwohnung mit der gewünschent Auswal kostet: \n" + String.valueOf(ergebnis) + "€");
+
+        }
+    }//GEN-LAST:event_bt_fw_berechnekostenActionPerformed
 
     /**
      * @param args the command line arguments
@@ -329,18 +461,24 @@ public class GUI extends javax.swing.JFrame {
         });
     }
 
-    public void buttons() {
-    }
-
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton bt_fw_berechnekosten;
     private javax.swing.JButton bt_z_berechnekosten;
+    private javax.swing.JCheckBox cb_fw_behindert;
+    private javax.swing.JCheckBox cb_fw_fruehstueck;
     private javax.swing.JCheckBox cb_z_behindert;
     private javax.swing.JCheckBox cb_z_einzelzimmer;
     private javax.swing.JCheckBox cb_z_fruehstueck;
     private javax.swing.JCheckBox cb_z_meerblick;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel11;
+    private javax.swing.JLabel jLabel12;
+    private javax.swing.JLabel jLabel13;
+    private javax.swing.JLabel jLabel14;
+    private javax.swing.JLabel jLabel15;
+    private javax.swing.JLabel jLabel16;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
@@ -352,10 +490,16 @@ public class GUI extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTabbedPane jTabbedPane1;
     private javax.swing.JLabel lb_z_error;
+    private javax.swing.JTextArea txa_fw_anzeigekosten;
     private javax.swing.JTextArea txa_z_anzeigekosten;
     private javax.swing.JTextField txf_fruehstueckspreis;
+    private javax.swing.JTextField txf_fw_anzahlpers;
+    private javax.swing.JTextField txf_fw_anzahlwochen;
+    private javax.swing.JTextField txf_fw_gebuehr;
+    private javax.swing.JTextField txf_fwnr;
     private javax.swing.JTextField txf_z_anzahltage;
     private javax.swing.JTextField txf_zimmernr;
     // End of variables declaration//GEN-END:variables
