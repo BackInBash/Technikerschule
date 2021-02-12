cad93c955599cdac26168717aa15b8259d802fde
Markus <48181660+BackInBash@users.noreply.github.com>
2021-02-12 16:44:22 +0100

    Update main.asm

--- Jahr 1/MC/Programmierung/HelloASM/HelloASM/main.asm
+++ Jahr 1/MC/Programmierung/HelloASM/HelloASM/main.asm
@@ -5,9 +5,18 @@
 ; Author : Markus
 ;
 
+ldi r16, 0b00000111
 ldi r22, 0x15
 
 ; Replace with your application code
+
+ldi r17, 0x1
+ldi r18, 0x1
+ldi r19, 0x0
+
+
 start:
     inc r16
+	mov r17, r18
+	add r18, r19
     rjmp start
