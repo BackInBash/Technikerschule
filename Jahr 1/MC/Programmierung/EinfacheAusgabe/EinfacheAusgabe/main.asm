0b870ac832a81a44c856e1addd850d59413e132d
Markus <48181660+BackInBash@users.noreply.github.com>
2021-03-02 13:57:11 +0100

    Update main.asm

--- Jahr 1/MC/Programmierung/EinfacheAusgabe/EinfacheAusgabe/main.asm
+++ Jahr 1/MC/Programmierung/EinfacheAusgabe/EinfacheAusgabe/main.asm
@@ -10,26 +10,26 @@
 ; LED an PA0
 
 ;====Beginn des Programms====
-.INCLUDE "m16def.inc" 		; Assembleranweisung: Definitionsdatei für ATmega16
-							; einfügen.
+.INCLUDE "m16def.inc" 		                        ; Assembleranweisung: Definitionsdatei fÃ¼r ATmega16
+							; einfÃ¼gen.
 							; Beinhaltet Namen zur einfacheren Programmierung
-							; z. B. PORTA für das Ausgangsregister des Port A
+							; z. B. PORTA fÃ¼r das Ausgangsregister des Port A
 							; .DEF PORTA = 0x1b (Speicheradresse)
 
 ;==== Initialisierung====
-init:						; Sprungmarke/Label
+init:						        ; Sprungmarke/Label
 							; Port A als Ausgang
-		ldi R16, 0xff		; Assemblerbefehl „load immediate“
+		ldi R16, 0xff		                ; Assemblerbefehl "load immediate"
 							; Register R16 mit dem Wert 0xff laden
-		out DDRA, R16		; Assemblerbefehl „out“:
+		out DDRA, R16		                ; Assemblerbefehl "out":
 							; Inhalt von R16 wird nach I/O-Register DDRA ausgegeben
 							; DDR-Register: Information, ob Aus- oder Eingang
 
 ;====Hauptprogramm====
 main:
-		sbi PORTA, 0		; Assemblerbefehl „set bit in I/O-Register“
-							; Register PORTA – PA0 auf 1 setzen
+		sbi PORTA, 0		                ; Assemblerbefehl "set bit in I/O-Register"
+							; Register PORTA â€“ PA0 auf 1 setzen
 							; Achtung: Befehl funktioniert nur mit den ersten 32
 							; I/O-Registern
 ende:
-		jmp ende			; Endlosschleife
+		jmp ende			       ; Endlosschleife
