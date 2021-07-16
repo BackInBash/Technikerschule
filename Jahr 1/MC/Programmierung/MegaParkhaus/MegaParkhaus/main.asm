f904e06e1056b1c8147877bc20b9bd622df0e0f3
Markus <48181660+BackInBash@users.noreply.github.com>
2021-07-16 14:37:09 +0200

    Update main.asm

--- Jahr 1/MC/Programmierung/MegaParkhaus/MegaParkhaus/main.asm
+++ Jahr 1/MC/Programmierung/MegaParkhaus/MegaParkhaus/main.asm
@@ -1,7 +1,7 @@
 ; MegaParkhaus.asm
 ;
-; Created: 02.07.2021 14:08:49
-; Author : simon.stadtmueller
+; Created: 16.07.2021 14:08:49
+; Author : markus.brunsch
 ;
 .INCLUDE "m16def.inc"
 
@@ -30,31 +30,53 @@ main:
 	in R20, PIND
 	andi R20, 0b00000100 ; Bit Mask Geschlossen
 	subi R20, 0b00000100 ; R20 - 1 = 0
-	brbc 1, main ; Jump if not Zero
+	brbc 1, closed ; Jump if not Zero
 
 	; Abgasalarm Einlesen
 	in R21, PIND
 	andi R21, 0b00001000 ; Bit Mask Abgasalarm
-	subi R21, 0b000001000 ; R21 - 1 = 0
-	brbc 1, main ; Jump If not Zero
+	subi R21, 0b00001000 ; R21 - 1 = 0
+	brbc 1, alarm ; Jump If not Zero
 
 	; Belegt Berechnen
 	andi R22, 0xF ; Check If R22 is 15 (ParkHaus voll)
-	brbs 1, einfahrt ; Jump if not full
-	rjmp main
+	brbc 1, full ; Jump if full
+	rjmp einfahrt
+
+	closed:
+		ldi R18, 0b10000000
+		out PORTA, R18
+		rjmp main
+
+	alarm:
+		ldi R18,  0b01000000
+		out PORTA, R18
+		rjmp main
+
+	full:
+		ldi R18, 0b00100000
+		out PORTA, R18
+		rjmp main
 
 einfahrt:
+	out PORTA, R16 ; Clear LEDs
 	ldi R17, 0x00 ; Set Local 
 	in R17, PIND  ; Load Value PINA
 
 	; TasterEinfahrtZuweisung
 	; R17 0bX0000000
 	andi R17, 0b10000000
-	brbc 1, einfahrt ; Jump if not Zero
+	brbc 1, main ; Jump if not Zero
 
-	in R17, PIND
-	andi R17, 0b01000000
+	wait:
+	in R17, PIND ; Read Lichtschranke
+	andi R17, 0b01000000 ; Mask Lichtschranke
+	brbc 1, wait ; Loop Lichtschranke
 	
+	inc R22 ; Increase Belegt Counter
+
+
+
 	;subi R17, R20 ; Einfahrt - Geschlossen
 	;brbc 1, R17
 
@@ -67,4 +89,6 @@ einfahrt:
 
 	istGeschlossen:
 
+
 ausfahrt:
+
