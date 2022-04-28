6cc184bacbaa35b47fdefc4609d68bfb48b3ba98
Markus <48181660+BackInBash@users.noreply.github.com>
2022-04-28 17:07:14 +0200

    Update transaction.sql

--- Jahr 2/DB/transaction.sql
+++ Jahr 2/DB/transaction.sql
@@ -1,10 +1,13 @@
 CREATE PROCEDURE transaction()
 BEGIN
+    # SupplierID Variabel
 	DECLARE SID INTEGER DEFAULT 1;
 	START TRANSACTION;
 	
+    # Disable Autocommit
 	SET AUTOCOMMIT = OFF;
 	
+    # Select SupplierID from Name
 	SELECT 
 	    SupplierID
 	INTO
@@ -14,14 +17,18 @@ BEGIN
 	WHERE
 	    CompanyName = "Nord-Ost-Fisch Handelsgesellschaft mbH";
 	
+    # Insert new Product with Supplier Infos
 	INSERT INTO products
 	    (ProductName, SupplierID, CategoryID, QuantityPerUnit, UnitPrice, UnitsInStock, UnitsOnOrder, ReorderLevel, Discontinued)
 	VALUES
 	    ('Lakritz', SID, 3, 10, 1.2, 100, 0, 10, 0);
 	
+    # Check if SupplierID is valid
 	IF SID > 1 THEN
+        # Commit Changes
 	    COMMIT;
 	ELSE
+        # Rollback Changes
 		ROLLBACK;
 	END IF;
 END
